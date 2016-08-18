67d3c11af113375f067349ada83a10a3ea88a022
Itanor Strapazzon <itanor@gmail.com>
2016-08-18 00:50:55 -0300

    some basic types

--- simple.exs
+++ simple.exs
@@ -1,2 +1,7 @@
-IO.puts "Hello world from Elixir"
+IO.puts "Hello world from Elixir" # string
+IO.puts 1                         # integer
+IO.puts 0x1F                      # integer
+IO.puts 1.0                       # float
+IO.puts true                      # boolean
+IO.puts :atom                     # atom / symbol
 
