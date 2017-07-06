dd0d552dccc9bf2fb31918abf44f2dfd97d57f8b
Itanor Strapazzon <itanor@gmail.com>
2017-07-06 08:38:50 -0300

    some map stuff

---
+++ map.exs
@@ -0,0 +1,53 @@
+ExUnit.start
+
+defmodule MapTest do
+  use ExUnit.Case
+
+  def sample do
+    %{foo: 'bar', baz: 'quiz'}
+  end
+
+  test "Map.get" do
+    assert Map.get(sample, :foo) == 'bar'
+    assert Map.get(sample, :non_existent) == nil
+  end
+
+  test "[]" do
+    assert sample[:foo] == 'bar'
+    assert sample[:non_existent] == nil
+  end
+
+  test "." do
+    assert sample.foo == 'bar'
+    assert_raise KeyError, fn ->
+      sample.non_existet
+    end
+  end
+
+  test "Map.fetch" do
+    {:ok, val} = Map.fetch(sample, :foo)
+    assert val == 'bar'
+    :error = Map.fetch(sample, :non_existet)
+  end
+
+  test "Map.put" do
+    assert Map.put(sample, :foo, 'bob') == %{foo: 'bob', baz: 'quiz'}
+    assert Map.put(sample, :far, 'bar') == %{foo: 'bar', baz: 'quiz', far: 'bar'}
+  end
+
+  test "Update map using pattern matching syntax" do
+    # You con only update existing keys in this way
+    assert %{sample | foo: 'bob'} == %{foo: 'bob', baz: 'quiz'}
+    # It doesn't work if you want to add new keys
+    assert_raise KeyError, fn ->
+      %{sample | far: 'bob'}
+    end
+  end
+
+  test "Map.values" do
+    # Map does not preserve order of keys, thus we Enum.sort
+    assert Enum.sort(Map.values(sample)) == ['bar', 'quiz']
+  end
+
+end
+
